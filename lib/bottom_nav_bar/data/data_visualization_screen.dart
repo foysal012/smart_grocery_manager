6fd149b3cdc8d514ef4b989d0fcec059f732ac37
Foysal <foysaljoarder012@gmail.com>
2026-05-01 23:36:03 +0600

    Working on visualization part

--- lib/bottom_nav_bar/data/data_visualization_screen.dart
+++ lib/bottom_nav_bar/data/data_visualization_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class DataVisualizationScreen extends StatefulWidget {
   const DataVisualizationScreen({super.key});
@@ -9,8 +10,31 @@ class DataVisualizationScreen extends StatefulWidget {
 }
 
 class _DataVisualizationScreenState extends State<DataVisualizationScreen> {
+  
+  late TooltipBehavior _tooltipBehavior;
 
-  bool showAvg = true;
+  @override
+  void initState(){
+    _tooltipBehavior = TooltipBehavior(enable: true);
+    super.initState();
+  }
+
+  final List<ChartData> chartData = [
+    ChartData(2, 8),
+    ChartData(4, 4),
+    ChartData(6, 6),
+    ChartData(8, 4),
+    ChartData(10, 8),
+    ChartData(12, 10),
+    ChartData(14, 6),
+  ];
+
+  final List<ChartData1> chartData1 = [
+    ChartData1('David', 25),
+    ChartData1('Steve', 38),
+    ChartData1('Jack', 34),
+    ChartData1('Others', 52)
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -20,325 +44,153 @@ class _DataVisualizationScreenState extends State<DataVisualizationScreen> {
         decoration: BoxDecoration(
           color: Color(0xffECEDF7)
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Gap(40.0),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      height: 50,
-                      width: 50,
-                      padding: EdgeInsets.all(2.5),
-                      decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                              width: 2,
-                              color: Color(0xff005BBF)
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gap(40.0),
+          
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 50,
+                        padding: EdgeInsets.all(2.5),
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                                width: 2,
+                                color: Color(0xff005BBF)
+                            ),
+                            image: DecorationImage(image: AssetImage('assets/images/grocery_cart_light.png'))
+                        ),
+                      ),
+                      Gap(10.0),
+          
+                      Text('Smart Grocery Manager',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Color(0xff1A73E8),
+                            fontWeight: FontWeight.bold
+                        ),
+                      )
+                    ],
+                  ),
+          
+                  Icon(Icons.notifications, size: 25.0)
+                ],
+              ),
+              Gap(16.0),
+          
+              Text('Analytics\nDashboard',
+                style: TextStyle(
+                  fontSize: 36,
+                  color: Color(0xff191C23),
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              Gap(8.0),
+          
+              Text('Deep insights into your inventory and\nfinancial performance.',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Color(0xff414754),
+                  fontWeight: FontWeight.normal
+                ),
+              ),
+              Gap(20.0),
+          
+              Container(
+                  child: SfCartesianChart(
+                      series: <CartesianSeries>[
+                        AreaSeries<ChartData, double>(
+                          dataSource: chartData,
+                          xValueMapper: (ChartData data, _) => data.x,
+                          yValueMapper: (ChartData data, _) => data.y,
+                          borderWidth: 4,
+                          borderGradient: const LinearGradient(
+                              colors: <Color>[
+                                Color.fromRGBO(230, 0, 180, 1),
+                                Color.fromRGBO(255, 200, 0, 1)
+                              ],
+                              stops: <double>[
+                                0.2,
+                                0.9
+                              ]
                           ),
-                          image: DecorationImage(image: AssetImage('assets/images/grocery_cart_light.png'))
-                      ),
-                    ),
-                    Gap(10.0),
-
-                    Text('Smart Grocery Manager',
-                      style: TextStyle(
-                          fontSize: 20.0,
-                          color: Color(0xff1A73E8),
-                          fontWeight: FontWeight.bold
-                      ),
-                    )
-                  ],
-                ),
-
-                Icon(Icons.notifications, size: 25.0)
-              ],
-            ),
-            Gap(16.0),
-
-            Text('Analytics\nDashboard',
-              style: TextStyle(
-                fontSize: 36,
-                color: Color(0xff191C23),
-                fontWeight: FontWeight.bold
+                        )
+                      ]
+                  )
               ),
-            ),
-            Gap(8.0),
+              Gap(20.0),
+          
+              Container(
+                child: SfCartesianChart(
+                    primaryXAxis: CategoryAxis(),
+                    title: ChartTitle(text: 'Half yearly sales analysis'),
+                    legend: Legend(isVisible: true),
+                    tooltipBehavior: _tooltipBehavior,
 
-            Text('Deep insights into your inventory and\nfinancial performance.',
-              style: TextStyle(
-                fontSize: 16,
-                color: Color(0xff414754),
-                fontWeight: FontWeight.normal
+                    series: <LineSeries<SalesData, String>>[
+                      LineSeries<SalesData, String>(
+                          dataSource:  <SalesData>[
+                            SalesData('Jan', 35),
+                            SalesData('Mar', 28),
+                            SalesData('May', 37),
+                            SalesData('Jul', 30),
+                            SalesData('Sep', 39),
+                            SalesData('Nov', 26),
+                          ],
+                          xValueMapper: (SalesData sales, _) => sales.year,
+                          yValueMapper: (SalesData sales, _) => sales.sales,
+                          dataLabelSettings: DataLabelSettings(isVisible: true)
+                      )
+                    ]
+                )
               ),
-            ),
-            Gap(40.0),
+              Gap(20.0),
 
-            Stack(
-              children: <Widget>[
-                AspectRatio(
-                  aspectRatio: 1.70,
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      right: 18,
-                      left: 12,
-                      top: 24,
-                      bottom: 12,
-                    ),
-                    child: LineChart(
-                      showAvg ? avgData() : mainData(),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 60,
-                  height: 34,
-                  child: TextButton(
-                    onPressed: () {
-                      setState(() {
-                        showAvg = !showAvg;
-                      });
-                    },
-                    child: Text(
-                      'avg',
-                      style: TextStyle(
-                        fontSize: 12,
-                        color: showAvg
-                            ? Colors.white.withValues(alpha: 0.5)
-                            : Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            )
-          ],
+              Container(
+                  child: SfCircularChart(
+                      series: <CircularSeries>[
+                        // Render pie chart
+                        PieSeries<ChartData1, String>(
+                            dataSource: chartData1,
+                            pointColorMapper:(ChartData1 data, _) => data.color,
+                            xValueMapper: (ChartData1 data, _) => data.x,
+                            yValueMapper: (ChartData1 data, _) => data.y
+                        )
+                      ]
+                  )
+              )
+          
+              
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-Widget bottomTitleWidgets(double value, TitleMeta meta) {
-  const style = TextStyle(
-    fontWeight: FontWeight.bold,
-    fontSize: 16,
-  );
-  String text = switch (value.toInt()) {
-    2 => 'MAR',
-    5 => 'JUN',
-    8 => 'SEP',
-    _ => '',
-  };
-  return SideTitleWidget(
-    meta: meta,
-    child: Text(text, style: style),
-  );
+class ChartData {
+  ChartData(this.x, this.y);
+  final double x;
+  final double? y;
 }
 
-Widget leftTitleWidgets(double value, TitleMeta meta) {
-  const style = TextStyle(
-    fontWeight: FontWeight.bold,
-    fontSize: 15,
-  );
-  String text = switch (value.toInt()) {
-    1 => '10K',
-    3 => '30k',
-    5 => '50k',
-    _ => '',
-  };
-
-  return Text(text, style: style, textAlign: TextAlign.left);
+class SalesData {
+  SalesData(this.year, this.sales);
+  final String year;
+  final double sales;
 }
 
-LineChartData mainData() {
-  return LineChartData(
-    gridData: FlGridData(
-      show: true,
-      drawVerticalLine: true,
-      horizontalInterval: 1,
-      verticalInterval: 1,
-      getDrawingHorizontalLine: (value) {
-        return const FlLine(
-          color: Colors.teal,
-          strokeWidth: 1,
-        );
-      },
-      getDrawingVerticalLine: (value) {
-        return const FlLine(
-          color: Colors.purple,
-          strokeWidth: 1,
-        );
-      },
-    ),
-    titlesData: FlTitlesData(
-      show: true,
-      rightTitles: const AxisTitles(
-        sideTitles: SideTitles(showTitles: false),
-      ),
-      topTitles: const AxisTitles(
-        sideTitles: SideTitles(showTitles: false),
-      ),
-      bottomTitles: AxisTitles(
-        sideTitles: SideTitles(
-          showTitles: true,
-          reservedSize: 30,
-          interval: 1,
-          getTitlesWidget: bottomTitleWidgets,
-        ),
-      ),
-      leftTitles: AxisTitles(
-        sideTitles: SideTitles(
-          showTitles: true,
-          interval: 1,
-          getTitlesWidget: leftTitleWidgets,
-          reservedSize: 42,
-        ),
-      ),
-    ),
-    borderData: FlBorderData(
-      show: true,
-      border: Border.all(color: const Color(0xff37434d)),
-    ),
-    minX: 0,
-    maxX: 11,
-    minY: 0,
-    maxY: 6,
-    lineBarsData: [
-      LineChartBarData(
-        spots: const [
-          FlSpot(0, 3),
-          FlSpot(2.6, 2),
-          FlSpot(4.9, 5),
-          FlSpot(6.8, 3.1),
-          FlSpot(8, 4),
-          FlSpot(9.5, 3),
-          FlSpot(11, 4),
-        ],
-        isCurved: true,
-        gradient: LinearGradient(
-          colors: gradientColors,
-        ),
-        barWidth: 5,
-        isStrokeCapRound: true,
-        dotData: const FlDotData(
-          show: false,
-        ),
-        belowBarData: BarAreaData(
-          show: true,
-          gradient: LinearGradient(
-            colors: gradientColors
-                .map((color) => color.withValues(alpha: 0.3))
-                .toList(),
-          ),
-        ),
-      ),
-    ],
-  );
+class ChartData1 {
+  ChartData1(this.x, this.y, [this.color]);
+  final String x;
+  final double y;
+  final Color? color;
 }
-
-LineChartData avgData() {
-  return LineChartData(
-    lineTouchData: const LineTouchData(enabled: false),
-    gridData: FlGridData(
-      show: true,
-      drawHorizontalLine: true,
-      verticalInterval: 1,
-      horizontalInterval: 1,
-      getDrawingVerticalLine: (value) {
-        return const FlLine(
-          color: Color(0xff37434d),
-          strokeWidth: 1,
-        );
-      },
-      getDrawingHorizontalLine: (value) {
-        return const FlLine(
-          color: Color(0xff37434d),
-          strokeWidth: 1,
-        );
-      },
-    ),
-    titlesData: FlTitlesData(
-      show: true,
-      bottomTitles: AxisTitles(
-        sideTitles: SideTitles(
-          showTitles: true,
-          reservedSize: 30,
-          getTitlesWidget: bottomTitleWidgets,
-          interval: 1,
-        ),
-      ),
-      leftTitles: AxisTitles(
-        sideTitles: SideTitles(
-          showTitles: true,
-          getTitlesWidget: leftTitleWidgets,
-          reservedSize: 42,
-          interval: 1,
-        ),
-      ),
-      topTitles: const AxisTitles(
-        sideTitles: SideTitles(showTitles: false),
-      ),
-      rightTitles: const AxisTitles(
-        sideTitles: SideTitles(showTitles: false),
-      ),
-    ),
-    borderData: FlBorderData(
-      show: true,
-      border: Border.all(color: const Color(0xff37434d)),
-    ),
-    minX: 0,
-    maxX: 11,
-    minY: 0,
-    maxY: 6,
-    lineBarsData: [
-      LineChartBarData(
-        spots: const [
-          FlSpot(0, 3.44),
-          FlSpot(2.6, 3.44),
-          FlSpot(4.9, 3.44),
-          FlSpot(6.8, 3.44),
-          FlSpot(8, 3.44),
-          FlSpot(9.5, 3.44),
-          FlSpot(11, 3.44),
-        ],
-        isCurved: true,
-        gradient: LinearGradient(
-          colors: [
-            ColorTween(begin: gradientColors[0], end: gradientColors[1])
-                .lerp(0.2)!,
-            ColorTween(begin: gradientColors[0], end: gradientColors[1])
-                .lerp(0.2)!,
-          ],
-        ),
-        barWidth: 5,
-        isStrokeCapRound: true,
-        dotData: const FlDotData(
-          show: false,
-        ),
-        belowBarData: BarAreaData(
-          show: true,
-          gradient: LinearGradient(
-            colors: [
-              ColorTween(begin: gradientColors[0], end: gradientColors[1])
-                  .lerp(0.2)!
-                  .withValues(alpha: 0.1),
-              ColorTween(begin: gradientColors[0], end: gradientColors[1])
-                  .lerp(0.2)!
-                  .withValues(alpha: 0.1),
-            ],
-          ),
-        ),
-      ),
-    ],
-  );
-}
-
-List<Color> gradientColors = [
-  Colors.cyan,
-  Colors.blue
-];
