56ca44784a389205bd0e0a43de7f91b67120eaab
Foysal <foysaljoarder012@gmail.com>
2026-05-20 23:07:23 +0600

    add new stock screen work 80% finished

--- lib/bottom_nav_bar/stock/add_new_stock_screen.dart
+++ lib/bottom_nav_bar/stock/add_new_stock_screen.dart
@@ -9,6 +9,13 @@ class AddNewStockScreen extends StatefulWidget {
 }
 
 class _AddNewStockScreenState extends State<AddNewStockScreen> {
+
+  final productNameText = TextEditingController();
+  final qtyText = TextEditingController();
+  final buyingPriceText = TextEditingController();
+  final sellingPriceText = TextEditingController();
+  final expiryDateText = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -126,10 +133,340 @@ class _AddNewStockScreenState extends State<AddNewStockScreen> {
                 ),
               ),
               Gap(40.0),
+              
+              Container(
+                padding: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  color: Colors.white
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          height: 30,
+                          width: 6,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                            color: Color(0xff005BBF),
+                          ),
+                        ),
+                        Gap(8.0),
+
+                        Text('Product Identity',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold
+                          ),
+                        )
+                      ],
+                    ),
+                    Gap(20.0),
+
+                    Text('Product Name',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.w500
+                      ),
+                    ),
+                    Gap(5.0),
+                    
+                    CustomTextFromField(
+                        productNameText: productNameText,
+                        hintText: 'e.g. Premium Industrial Hub',
+                    ),
+                    Gap(10.0),
+
+                    Text('Quantity',
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black54,
+                          fontWeight: FontWeight.w500
+                      ),
+                    ),
+                    Gap(5.0),
+
+                    CustomTextFromField(
+                      productNameText: qtyText,
+                      hintText: '000',
+                      suffixWidget: Text('UNITS',
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Color(0xff727785),
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                    Gap(20.0),
+
+                    Row(
+                      children: [
+                        Container(
+                          height: 30,
+                          width: 6,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                            color: Color(0xff9E4300),
+                          ),
+                        ),
+                        Gap(8.0),
+
+                        Text('Financial Parameters',
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold
+                          ),
+                        )
+                      ],
+                    ),
+                    Gap(20.0),
+
+                    Text('Buying Price',
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black54,
+                          fontWeight: FontWeight.w500
+                      ),
+                    ),
+                    Gap(5.0),
+
+                    CustomTextFromField(
+                      productNameText: buyingPriceText,
+                      hintText: '0.00',
+                      prefixWidget: Icon(Icons.attach_money_outlined, color: Color(0xff005BBF)),
+                    ),
+                    Gap(10.0),
+
+                    Text('selling Price',
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black54,
+                          fontWeight: FontWeight.w500
+                      ),
+                    ),
+                    Gap(5.0),
+
+                    CustomTextFromField(
+                      productNameText: sellingPriceText,
+                      hintText: '0.00',
+                      prefixWidget: Icon(Icons.attach_money_outlined, color: Colors.green),
+                    ),
+                    Gap(20.0),
+
+                    Row(
+                      children: [
+                        Container(
+                          height: 30,
+                          width: 6,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                            color: Colors.green,
+                          ),
+                        ),
+                        Gap(8.0),
+
+                        Text('Compliance & Lifecycle',
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold
+                          ),
+                        )
+                      ],
+                    ),
+                    Gap(20.0),
+
+                    Text('Expiry Date',
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black54,
+                          fontWeight: FontWeight.w500
+                      ),
+                    ),
+                    Gap(5.0),
+
+                    CustomTextFromField(
+                      productNameText: buyingPriceText,
+                      hintText: 'mm/dd/yyyy',
+                      suffixWidget: Icon(Icons.calendar_month_outlined, color: Color(0xff727785)),
+                    ),
+                    Gap(40.0),
+
+                    Container(
+                      height: 52,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        color: Color(0xff005BBF)
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Save Stock',
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          Gap(10.0),
+
+                          Icon(Icons.send, color: Colors.white,)
+                        ],
+                      ),
+                    ),
+                    Gap(20.0)
+                  ],
+                ),
+              ),
+              Gap(20.0),
+
+              Container(
+                height: 240,
+                padding: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  color: Color(0xff86F898).withValues(alpha: 0.30)
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                            padding: EdgeInsets.all(10.0),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.green
+                            ),
+                            child: Icon(Icons.done_outline, color: Colors.white)
+                        ),
+                        Gap(10.0),
+
+                        Text('Return Success',
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Color(0xff065A0A),
+                              fontWeight: FontWeight.bold
+                          ),
+                        )                      ],
+                    ),
+                    Gap(10.0),
+
+                    Container(
+                      height: 120,
+                      padding: EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          color: Colors.white
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('ENTRY CONFIRMED',
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: Color(0xff065A0A),
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          Gap(10.0),
+                          
+                          Text('Precision Hex Bolt X12',
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          Gap(15.0),
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                padding: EdgeInsets.symmetric(horizontal: 5, vertical: 2.5),
+                                  decoration: BoxDecoration(
+                                    color: Color(0xffF2F3FD),
+                                    borderRadius: BorderRadius.all(Radius.circular(4.0))
+                                  ),
+                                  child: Text('Qty: 500',
+                                    style: TextStyle(
+                                      color: Color(0xff414754)
+                                    ),
+                                  )),
+                              
+                              Text('Id: #99281 PL')
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    Gap(15.0),
+
+                    Text('Successfully published to regional node',
+                      style: TextStyle(
+                          fontSize: 14,
+                          color: Color(0xff065A0A),
+                          fontWeight: FontWeight.bold
+                      ),
+                    )
+
+                  ],
+                ),
+              ),
+              Gap(20.0),
 
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class CustomTextFromField extends StatelessWidget {
+  const CustomTextFromField({
+    super.key,
+    required this.productNameText,
+    this.hintText,
+    this.suffixWidget,
+    this.prefixWidget,
+  });
+
+  final TextEditingController productNameText;
+  final String? hintText;
+  final Widget? suffixWidget;
+  final Widget? prefixWidget;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: productNameText,
+      decoration: InputDecoration(
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          borderSide: BorderSide(color: Colors.transparent)
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          borderSide: BorderSide(color: Colors.transparent)
+        ),
+        hintText: hintText,
+        hintStyle: TextStyle(
+          fontSize: 14,
+          color: Color(0xff727785),
+          fontWeight: FontWeight.bold
+        ),
+        fillColor: Color(0xffE0E2EC),
+        filled: true,
+        suffixIcon: suffixWidget,
+        suffixIconConstraints: BoxConstraints.expand(height: 25, width: 60),
+        prefixIcon: prefixWidget
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Smart Grocery Manager',
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
